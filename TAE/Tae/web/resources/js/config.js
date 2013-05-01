bd78769e58b039b6e60dfae1e7c0c11e37f1b4b1
Héctor Acosta <hacosta@ihsa.mx>
2013-04-30 19:31:32 -0500

    Menu agregado

---
+++ TAE/Tae/web/resources/js/config.js
@@ -0,0 +1,4 @@
+FCKConfig.ToolbarSets["TOOL_BAR"] = [['FontSize','Bold','Italic', 'Underline','-','TextColor','BGColor'],['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+['Table','Rule','Smiley','Link'],] ;
+
+FCKConfig.ToolbarSets["NONE"] = []; 
